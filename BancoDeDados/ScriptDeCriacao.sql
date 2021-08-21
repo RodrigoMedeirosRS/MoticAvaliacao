18fce9cb7ff7f1ea4113869d7a4e3c8bd78e4a7c
RodrigoMedeirosRS <rodrigo.medeiros@gmail.com>
2021-08-21 20:14:17 -0300

    feat(estrutura banco de dados)

---
+++ BancoDeDados/ScriptDeCriacao.sql
@@ -0,0 +1,89 @@
+CREATE TABLE categoria (
+  codigo SERIAL  NOT NULL ,
+  nome INTEGER   NOT NULL   ,
+PRIMARY KEY(codigo));
+
+
+
+
+CREATE TABLE nomecriterio (
+  codigo SERIAL  NOT NULL ,
+  nome VARCHAR(100)   NOT NULL ,
+  peso INTEGER   NOT NULL   ,
+PRIMARY KEY(codigo));
+
+
+
+
+CREATE TABLE avaliador (
+  codigo SERIAL  NOT NULL ,
+  nome VARCHAR(50)   NOT NULL ,
+  sobrenome VARCHAR(150)   NOT NULL ,
+  cpf VARCHAR(11)   NOT NULL ,
+  senha VARCHAR(20)   NOT NULL ,
+  email VARCHAR(100)    ,
+  telefone VARCHAR(20)      ,
+PRIMARY KEY(codigo));
+
+
+
+
+CREATE TABLE trabalho (
+  codigo SERIAL  NOT NULL ,
+  categoria INTEGER   NOT NULL ,
+  nome VARCHAR(200)   NOT NULL ,
+  escola VARCHAR(200)   NOT NULL ,
+  anoapresentacao DATE   NOT NULL   ,
+PRIMARY KEY(codigo)  ,
+  FOREIGN KEY(categoria)
+    REFERENCES categoria(codigo));
+
+
+CREATE INDEX trabalho_FKIndex1 ON trabalho (categoria);
+
+
+CREATE INDEX IFK_Rel_03 ON trabalho (categoria);
+
+
+CREATE TABLE avaliacao (
+  codigo SERIAL  NOT NULL ,
+  avaliador INTEGER   NOT NULL ,
+  trabalho INTEGER   NOT NULL   ,
+PRIMARY KEY(codigo)    ,
+  FOREIGN KEY(avaliador)
+    REFERENCES avaliador(codigo),
+  FOREIGN KEY(trabalho)
+    REFERENCES trabalho(codigo));
+
+
+CREATE INDEX avaliador_trabalho_FKIndex1 ON avaliacao (avaliador);
+CREATE INDEX avaliador_trabalho_FKIndex2 ON avaliacao (trabalho);
+
+
+CREATE INDEX IFK_Rel_01 ON avaliacao (avaliador);
+CREATE INDEX IFK_Rel_02 ON avaliacao (trabalho);
+
+
+CREATE TABLE criterio (
+  codigo SERIAL  NOT NULL ,
+  avaliacao INTEGER   NOT NULL ,
+  nomecriterio INTEGER   NOT NULL ,
+  descricao VARCHAR(500)    ,
+  observacao VARCHAR(500)    ,
+  nota FLOAT      ,
+PRIMARY KEY(codigo)    ,
+  FOREIGN KEY(nomecriterio)
+    REFERENCES nomecriterio(codigo),
+  FOREIGN KEY(avaliacao)
+    REFERENCES avaliacao(codigo));
+
+
+CREATE INDEX criterio_FKIndex1 ON criterio (nomecriterio);
+CREATE INDEX criterio_FKIndex2 ON criterio (avaliacao);
+
+
+CREATE INDEX IFK_Rel_04 ON criterio (nomecriterio);
+CREATE INDEX IFK_Rel_09 ON criterio (avaliacao);
+
+
+
---
+++ BancoDeDados/ScriptDeDrop.sql
@@ -0,0 +1,13 @@
+DROP TABLE criterio;
+
+DROP TABLE avaliacao;
+
+DROP TABLE trabalho;
+
+DROP TABLE avaliador;
+
+DROP TABLE nomecriterio;
+
+DROP TABLE categoria;
+
+
