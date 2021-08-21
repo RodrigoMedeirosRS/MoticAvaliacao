f2248a17ec3d1922ea95d28827ef736a54dffee8
RodrigoMedeirosRS <rodrigo.medeiros@gmail.com>
2021-08-21 20:21:19 -0300

    feat(ajustes banco)

--- BancoDeDados/ScriptDeCriacao.sql
+++ BancoDeDados/ScriptDeCriacao.sql
@@ -1,49 +1,36 @@
 CREATE TABLE categoria (
   codigo SERIAL  NOT NULL ,
-  nome INTEGER   NOT NULL   ,
+  nome INTEGER   UNIQUE   NOT NULL   ,
 PRIMARY KEY(codigo));
-
-
-
 
 CREATE TABLE nomecriterio (
   codigo SERIAL  NOT NULL ,
-  nome VARCHAR(100)   NOT NULL ,
+  nome VARCHAR(100)   UNIQUE   NOT NULL ,
   peso INTEGER   NOT NULL   ,
 PRIMARY KEY(codigo));
-
-
-
 
 CREATE TABLE avaliador (
   codigo SERIAL  NOT NULL ,
   nome VARCHAR(50)   NOT NULL ,
   sobrenome VARCHAR(150)   NOT NULL ,
-  cpf VARCHAR(11)   NOT NULL ,
+  cpf VARCHAR(11)   UNIQUE   NOT NULL ,
   senha VARCHAR(20)   NOT NULL ,
   email VARCHAR(100)    ,
   telefone VARCHAR(20)      ,
 PRIMARY KEY(codigo));
 
-
-
-
 CREATE TABLE trabalho (
   codigo SERIAL  NOT NULL ,
   categoria INTEGER   NOT NULL ,
-  nome VARCHAR(200)   NOT NULL ,
+  nome VARCHAR(200)   UNIQUE   NOT NULL ,
   escola VARCHAR(200)   NOT NULL ,
-  anoapresentacao DATE   NOT NULL   ,
+  anoapresentacao TIMESTAMP   NOT NULL   ,
 PRIMARY KEY(codigo)  ,
   FOREIGN KEY(categoria)
     REFERENCES categoria(codigo));
 
-
 CREATE INDEX trabalho_FKIndex1 ON trabalho (categoria);
-
-
 CREATE INDEX IFK_Rel_03 ON trabalho (categoria);
-
 
 CREATE TABLE avaliacao (
   codigo SERIAL  NOT NULL ,
@@ -55,10 +42,8 @@ PRIMARY KEY(codigo)    ,
   FOREIGN KEY(trabalho)
     REFERENCES trabalho(codigo));
 
-
 CREATE INDEX avaliador_trabalho_FKIndex1 ON avaliacao (avaliador);
 CREATE INDEX avaliador_trabalho_FKIndex2 ON avaliacao (trabalho);
-
 
 CREATE INDEX IFK_Rel_01 ON avaliacao (avaliador);
 CREATE INDEX IFK_Rel_02 ON avaliacao (trabalho);
@@ -77,10 +62,8 @@ PRIMARY KEY(codigo)    ,
   FOREIGN KEY(avaliacao)
     REFERENCES avaliacao(codigo));
 
-
 CREATE INDEX criterio_FKIndex1 ON criterio (nomecriterio);
 CREATE INDEX criterio_FKIndex2 ON criterio (avaliacao);
-
 
 CREATE INDEX IFK_Rel_04 ON criterio (nomecriterio);
 CREATE INDEX IFK_Rel_09 ON criterio (avaliacao);
